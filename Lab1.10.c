6f020323567048033a7fb42537f9b03e09f6377b
NattanonDev <thg0772@gmail.com>
2025-11-24 14:30:11 +0700

    Update Lab1.10.c

--- Lab1.10.c
+++ Lab1.10.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
     int clearanceLevel, age, isActive;
 
-    if (scanf("%d %d %d", &clearanceLevel, &age, &isActive) != 3)
-        return 1;
-
-    int ok = 0;
-
-    if (clearanceLevel == 3 && isActive == 1)
-        ok = 1;
-    else if (clearanceLevel == 2)
-    {
-        if (age >= 25 && isActive == 1)
-            ok = 1;
+    if (scanf("%d %d %d", &clearanceLevel, &age, &isActive) != 3) {
+        return 1; 
     }
 
-    if (ok)
+    if ( (clearanceLevel == 3 && isActive == 1) ||
+         (clearanceLevel == 2 && age >= 25 && isActive == 1) ) 
+    {
         printf("Access Granted");
-    else
+    } else {
         printf("Access Denied");
+    }
 
     return 0;
 }
