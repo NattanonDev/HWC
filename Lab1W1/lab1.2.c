423c352f0270a58b0bbc1fa11cf6968cd2dd6c60
NattanonDev <thg0772@gmail.com>
2025-11-30 14:27:49 +0000

    Nov 30, 2025, 9:27 PM

---
+++ Lab1W1/lab1.2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void GetSet(int data[], int *n);
+
+void GetSet(int data[], int *n) {
+    int i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", n);
+
+    for (i = 0; i < *n; i++) {
+        printf("Enter value %d: ", i + 1);
+        scanf("%d", &data[i]);
+    }
+}
+
+int main() {
+    int *data, num;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &num);
+
+    data = (int *)malloc(num * sizeof(int));   // จองหน่วยความจำ
+
+    GetSet(data, &num);
+
+    printf("\n--- Result ---\n");
+    for (int i = 0; i < num; i++) {
+        printf("data[%d] = %d\n", i, data[i]);
+    }
+
+    free(data);  // คืนหน่วยความจำ
+    return 0;
+}
