6024dbdc6a12b77d0654cf6362f51bff829fb1e1
NattanonDev <thg0772@gmail.com>
2025-12-03 06:42:44 +0000

    Dec 3, 2025, 1:42 PM

--- Lab1W1/lab1.2.c
+++ Lab1W1/lab1.2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void GetSet(int **data, int *n);  // *** ต้องส่ง pointer to pointer
+void GetSet(int **data, int *n);
 
 void GetSet(int **data, int *n) {
     int i;
