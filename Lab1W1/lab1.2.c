bebf1a3cabeabc9a982fe47da3435560ec289ab1
NattanonDev <thg0772@gmail.com>
2025-12-03 06:42:29 +0000

    Dec 3, 2025, 1:42 PM

--- Lab1W1/lab1.2.c
+++ Lab1W1/lab1.2.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void GetSet(int data[], int *n);
+void GetSet(int **data, int *n);  // *** ต้องส่ง pointer to pointer
 
-void GetSet(int data[], int *n) {
+void GetSet(int **data, int *n) {
     int i;
 
     printf("Enter number of elements: ");
     scanf("%d", n);
 
+    *data = (int *)malloc(*n * sizeof(int));
+
     for (i = 0; i < *n; i++) {
         printf("Enter value %d: ", i + 1);
-        scanf("%d", &data[i]);
+        scanf("%d", &(*data)[i]);
     }
 }
 
 int main() {
-    int *data, num;
+    int *data = NULL;
+    int num;
 
-    // printf("Enter number of elements: ");
-    scanf("%d", &num);
-
-    data = (int *)malloc(num * sizeof(int));
-
-    GetSet(data, &num);
+    GetSet(&data, &num);
 
     printf("\n--- Result ---\n");
     for (int i = 0; i < num; i++) {
