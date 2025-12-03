93eac273be264242a40551a5adfab18863253d68
NattanonDev <thg0772@gmail.com>
2025-12-03 08:00:09 +0000

    Dec 3, 2025, 3:00 PM

--- Lab1W1/lab1.3.c
+++ Lab1W1/lab1.3.c
@@ -1,38 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int *GetSet(int *n)
-{
-    int i;
-    int *arr;
-
-    printf("Enter number of elements: ");
-    scanf("%d", n);
-
-    arr = (int *)malloc((*n) * sizeof(int));
-    if (arr == NULL) {
-        return NULL;
-    }
-
-    for(i = 0; i < *n; i++) {
-        printf("Enter value %d: ", i + 1);
-        scanf("%d", &arr[i]);
-    }
-
-    return arr;
-}
+int *GetSet(int *num);
 
 int main()
 {
-    int *data, num;
+    int *data;
+    int num;
     int i;
 
     data = GetSet(&num);
 
-    for(i = 0; i < num; i++) {
+    printf("\n--- Result ---\n");
+    for (i = 0; i < num; i++) {
         printf("data[%d] = %d\n", i, data[i]);
     }
 
     free(data);
     return 0;
+}
+
+int *GetSet(int *num)
+{
+    int i;
+    int *data;
+
+    printf("Enter number of elements: ");
+    if (scanf("%d", num) != 1) {
+        printf("No number!\n");
+        return NULL;
+    }
+
+    data = (int *)malloc((*num) * sizeof(int));
+    if (data == NULL) {
+        return NULL;
+    }
+
+    for (i = 0; i < *num; i++) {
+        printf("Enter Number %d: ", i + 1);
+        if (scanf("%d", &data[i]) != 1) {
+            printf("No number!\n");
+            free(data);
+            return NULL;
+        }
+    }
+
+    return data;
 }
