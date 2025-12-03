a17b26ea5eeb806c2886ed470c673e0734294dca
NattanonDev <thg0772@gmail.com>
2025-12-03 07:30:09 +0000

    Dec 3, 2025, 2:30 PM

--- Lab1W1/lab1.22.c
+++ Lab1W1/lab1.22.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void GetSet(int a[], int *n)
+void GetSet(int data[], int *num);
+
+int main()
+{
+    int data[100];
+    int num;
+
+    GetSet(data, &num);
+
+    return 0;
+}
+
+void GetSet(int data[], int *num)
 {
     int i;
 
     printf("Enter number of elements: ");
-    if (scanf("%d", n) != 1) {
-        printf("No number!\n");
+    if (scanf("%d", num) != 1) {
+        printf("No number goodbye!\n");
         return;
     }
 
-    for (i = 0; i < *n; i++) {
+    for (i = 0; i < *num; i++) {
         printf("Enter Number %d: ", i + 1);
-        if (scanf("%d", &a[i]) != 1) {
-            printf("No number!\n");
+        if (scanf("%d", &data[i]) != 1) {
+            printf("No number goodbye!\n");
             return;
         }
     }
 
     printf("\nYour numbers are:\n");
-    for (i = 0; i < *n; i++) {
-        printf("%d ", a[i]);
+    for (i = 0; i < *num; i++) {
+        printf("%d ", data[i]);
     }
     printf("\n");
-}
-
-int main()
-{
-    int *data, num;
-
-    data = (int *)malloc(100 * sizeof(int));
-
-    GetSet(data, &num);
-
-    free(data);
-    return 0;
 }
