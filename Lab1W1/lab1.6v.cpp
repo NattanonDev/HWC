f37683a3bf473087409e3e6f98794d967c7c4386
NattanonDev <thg0772@gmail.com>
2025-12-06 07:42:18 +0000

    Dec 6, 2025, 2:42 PM

--- Lab1W1/lab1.6v.cpp
+++ Lab1W1/lab1.6v.cpp
@@ -1,44 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int *GetMatrix(int *row, int *col);
 
-int main()
-{
-    int *data;
-    int m;
-    int n;
-    int i;
-    int j;
-
+int main(){
+    int *data, m , n;
     data = GetMatrix(&m, &n);
 
     printf("\nMatrix Input\n");
-
-    for (i = 0; i < m; i++) {
-        printf("%d row\n", i + 1);
-
-        for (j = 0; j < n; j++) {
-            printf("%d col number: ", j + 1);
-            scanf("%d", &data[i * n + j]);
+    for(int i = 0; i < m; i++){
+        printf("%d row\n",i+1);
+        for(int j = 0; j < n; j++){
+            printf("%d col number: ",j+1);
+            scanf("%d", &data[ i * n + j]);
         }
     }
-
     return 0;
 }
 
-int *GetMatrix(int *row, int *col)
-{
+int *GetMatrix(int *row, int *col){
     int *Pvar;
-    int total;
-
-    printf("How many row, col ? : ");
+    printf("How many row, col ? :");
     scanf("%d %d", row, col);
-
-    /* สร้างพื้นที่เก็บข้อมูลตามจำนวนที่ผู้ใช้ใส่ */
-    total = (*row) * (*col);
-
-    Pvar = (int *)malloc(total * sizeof(int));
-
+    Pvar = new int[ *row * *col];
     return Pvar;
 }
