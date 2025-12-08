67a697e44fe39d075789a2ff7c2ed5794e74c6b3
NattanonDev <thg0772@gmail.com>
2025-12-08 06:56:00 +0000

    Dec 8, 2025, 1:56 PM

--- Lab2W2/lab2.1.c
+++ Lab2W2/lab2.1.c
@@ -5,10 +5,10 @@ void reverse( char str1[], char str2[] ) {
     int n = strlen(str1);
     int j = 0;
 
-    for(int i = n-1 ; i >= 0 ; i--){
-        str2[j] = str1[i];
-        j++;
+    for(int i = n - 1; i >= 0; i--){
+        str2[j++] = str1[i];
     }
+
     str2[j] = '\0';
 }
 
@@ -16,7 +16,10 @@ int main() {
     char text[50];
     char out[50];
 
-    gets(text);
+    fgets(text, 50, stdin);
+
+    // ลบ newline ของ fgets ถ้ามี
+    text[strcspn(text, "\n")] = '\0';
 
     reverse(text, out);
 
