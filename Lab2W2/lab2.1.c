df332d458d717932d5b2a150411a1e1f0ffac322
NattanonDev <thg0772@gmail.com>
2025-12-08 06:44:12 +0000

    Dec 8, 2025, 1:44 PM

--- Lab2W2/lab2.1.c
+++ Lab2W2/lab2.1.c
@@ -1,17 +1,4 @@
 #include <stdio.h>
-
-void reverse( char str1[], char str2[] );
-
-int main() {
-    char text[50] = "I Love You";
-    char out[50];
-    reverse(text,out);
-
-    return 0;
-
-}
-
-#include <stdio.h>
 #include <string.h>
 
 void reverse(char str1[], char str2[]) {
