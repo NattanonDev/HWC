44c78510d832fdbd4b4ed7d2b5e9530cfb19daa3
NattanonDev <thg0772@gmail.com>
2025-12-08 07:13:48 +0000

    Dec 8, 2025, 2:13 PM

--- Lab2W2/lab2.1w2c.c
+++ Lab2W2/lab2.1w2c.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-void reverse( char str1[], char str2[] ) {
-    int n = strlen(str1);
+void reverse(char str1[], char str2[]) {
+    int len = strlen(str1);
     int j = 0;
 
-    for(int i = n - 1; i >= 0; i--){
-        str2[j++] = str1[i];
+    for (int i = len - 1; i >= 0; i--) {
+        str2[j] = str1[i];
+        j++;
     }
-
     str2[j] = '\0';
 }
 
 int main() {
-    char text[50];
+    char text[50] = "I Love You";
     char out[50];
-
-    fgets(text, 50, stdin);
-
-    // ลบ newline ของ fgets ถ้ามี
-    text[strcspn(text, "\n")] = '\0';
 
     reverse(text, out);
 
     printf("%s", out);
-
     return 0;
 }
