ef46be25667ecfa2f69131b8104b903e20c8a2ed
NattanonDev <thg0772@gmail.com>
2025-12-26 18:07:42 +0000

    Dec 27, 2025, 1:07 AM

--- Lab4W4/lab4.1w4.c
+++ Lab4W4/lab4.1w4.c
@@ -5,5 +5,8 @@ void go(int **p,int *z);
 int main() {
     int *a, b = 10, c = 20;
     go(&a,&b);
-    printf
+    printf("%d %p %p", *a, a, &a);
+    go(&a,&c);
+    printf("%d %p %p", *a, a, &a);
+    return 0;
 }
