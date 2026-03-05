4ffab2ec8660d88c8cbd8b54dd863877b9cbdcdf
NattanonDev <thg0772@gmail.com>
2026-03-05 16:55:00 +0000

    Mar 5, 2026, 11:55 PM

--- Lab10W10/Lab10_1.cpp
+++ Lab10W10/Lab10_1.cpp
@@ -1,6 +1,36 @@
-#include <iostream>
+#include <stdio.h>
 
+/* prototype */
+long SumLoop(int);
+long SumRecur(int);
+
+/* main */
 int main() {
-    std::cout << "Hello World!" << std::endl;
+
+    printf("SumLoop(n) = %ld\n", SumLoop(10));
+    printf("SumRecur(n) = %ld\n", SumRecur(10));
+
     return 0;
+}
+
+/* function แบบ loop */
+long SumLoop(int n) {
+
+    long sum = 0;
+    int i;
+
+    for(i = 1; i <= n; i++) {
+        sum = sum + i;
+    }
+
+    return sum;
+}
+
+/* function แบบ recursion */
+long SumRecur(int n) {
+
+    if(n == 1)      // base case
+        return 1;
+
+    return n + SumRecur(n - 1);
 }
